7157e7b04a60c629675a61f70b0a41a376249b48
Isabelle de Oliveira Moura <isabelle.moura@BRSAOMN45470.local>
2025-01-03 23:40:11 -0300

    build

---
+++ build/client/assets/config-CYgZ-SvK.js
@@ -0,0 +1,5 @@
+const s="Isabelle Oliveira",o="Designer",i=["Developer","Prototyper","Modder"],e="https://hamishw.com",t="hamishw.com",n="@Hamish",c="HamishMW",a="https://github.com/HamishMW/portfolio",r=`__  __  __
+\\ \\ \\ \\ \\∕
+ \\ \\∕\\ \\
+  \\∕  \\∕
+`,l={name:s,role:o,disciplines:i,url:e,bluesky:t,figma:n,github:c,repo:a,ascii:r};export{l as c};
